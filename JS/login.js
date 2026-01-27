34e12854d749314f6801340cf31bed471bf71b6c
Git Master <blah@blibidi.com>
2026-01-27 11:18:58 +0200

    sum small visual fixes

--- JS/login.js
+++ JS/login.js
@@ -4,16 +4,14 @@ document.getElementById('login-btn').addEventListener('click', () => {
     signInWithPopup(auth, provider)
         .then(async (result) => {
             const user = result.user;
-            
-            // 1. Check if this user already exists in our Database
+
             const userRef = ref(database, 'users/' + user.uid);
             
             try {
                 const snapshot = await get(userRef);
                 
                 if (!snapshot.exists()) {
-                    // 2. If NOT, create their initial skeleton data
-                    console.log("New user detected! Creating database profile...");
+                    //console.log("New user detected! Creating database profile...");
                     await set(userRef, {
                         email: user.email,
                         name: user.displayName,
@@ -21,12 +19,11 @@ document.getElementById('login-btn').addEventListener('click', () => {
                         totalLikes: 0,
                         accountCreated: new Date().toISOString(),
                         
-                        scans: {} // This stays as a sibling to the other data
+                        scans: {}
                     });
                 } else {
-                    console.log("Existing user found.");
+                    //console.log("Existing user found.");
                 }
-                
                 // 3. Proceed to the site
                 window.location.href = 'index.html';
                 
