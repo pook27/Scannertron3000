48e9ed1c67f45c0d2307166be32941a89c543952
Git Master <blah@blibidi.com>
2026-05-09 20:24:22 +0300

    added to github pages

---
+++ JS/firebase-config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAbE-csfySXiTfvcOPqSiL1U8Sdy33xDpg",
+  authDomain: "scannertron3k.firebaseapp.com",
+  databaseURL: "https://scannertron3k-default-rtdb.firebaseio.com", 
+  projectId: "scannertron3k",
+  storageBucket: "scannertron3k.firebasestorage.app",
+  messagingSenderId: "360137980867",
+  appId: "1:360137980867:web:210601fe0abfafdd279b76"
+};
